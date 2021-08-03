577d455f500fdac04bb02f79cd27fa76f6cf9895
Gerard Beck <gerardbeck@uchicago.edu>
2021-08-03 11:40:32 -0500

    Changed button placement and did slight refactoring.

--- speed_reader.py
+++ speed_reader.py
@@ -6,29 +6,28 @@ from termcolor import colored
 import tkinter as tk
 
 class Speed_reader:
-    def __init__(self, file_name=None, text_speed=500) -> None:
+    def __init__(self, file_name=None, refresh_rate=500) -> None:
         
-        if file_name == None:
-            self.waiting_for_file_flag = 1
-            self.file_name = None
-        else:
+        self.waiting_for_file_flag = 1
+        self.file_name = file_name
+
+        if self.file_name != None:
             self.waiting_for_file_flag = 0
-            self.file_name = file_name
             self.initialize_word_list()
 
-        self.sentence_index = 0
-        self.word_index = 0
+        #Setting default values for text output
+        self.sentence_index, self.word_index = 0, 0
         self.num_words_per_output = 3
         self.content = ""
-        self.end_of_sentence_flag = 0
+        self.end_of_sentence_flag = 0 
 
-        self.text_speed = text_speed
+        self.refresh_rate = refresh_rate
         self.pause_flag = 0
 
     def initialize_word_list(self):
         self.file_contents = self.read_file()
         self.all_sentences = self.file_into_sentences()
-        self.list_of_list_of_words = self.sentences_into_list_of_words()
+        self.list_of_list_of_words = self.sentences_into_word_lists()
 
     def read_file(self): 
         with open(self.file_name) as f: 
@@ -40,17 +39,16 @@ class Speed_reader:
     def sentence_into_words(self, sentence):
         return nltk.word_tokenize(sentence)
 
-    def sentences_into_list_of_words(self):
+    def sentences_into_word_lists(self):
         word_list = []
         for sentence in self.all_sentences : 
             word_list.append( self.sentence_into_words(sentence) )
         return word_list
 
     def get_output(self, sentence_index, word_index):
+        sentence = []
         if sentence_index < len(self.list_of_list_of_words):
             sentence = self.list_of_list_of_words[sentence_index]
-        else: 
-            sentence = []
 
         num_words_left = len(sentence) - word_index
 
@@ -74,11 +72,11 @@ class Speed_reader:
 
         return content, sentence_index, word_index, end_of_sentence_flag
 
-    def change_text_speed(self, speed_change):
-        if self.text_speed + speed_change > 0:
-            self.text_speed = self.text_speed + speed_change
+    def change_refresh_rate(self, speed_change):
+        if self.refresh_rate + speed_change > 0:
+            self.refresh_rate = self.refresh_rate + speed_change
 
-    def change_pause_flag(self):
+    def flip_pause_flag(self):
         self.pause_flag = not self.pause_flag
 
     def go_back_x_sentences(self, num_sentences=10):
@@ -102,31 +100,31 @@ class Speed_reader:
 
         def Load_file():
             file_button = tk.Button(root, text="Pick File", command= pick_file)
-            file_button.pack()
+            file_button.place(x=5, y=5)
 
         def Speed_up():
-            speed_up_button = tk.Button(root, text="Speed Up", command= lambda: self.change_text_speed(-25))
-            speed_up_button.pack()
+            speed_up_button = tk.Button(root, text="Speed Up", command= lambda: self.change_refresh_rate(-25))
+            speed_up_button.place(x=225, y=65)
 
         def Speed_down():
-            speed_down_button = tk.Button(root, text="Speed Down", command= lambda: self.change_text_speed(25))
-            speed_down_button.pack()
+            speed_down_button = tk.Button(root, text="Speed Down", command= lambda: self.change_refresh_rate(25))
+            speed_down_button.place(x=225, y=5)
 
         def Pause():
-            pause_button = tk.Button(root, text="Pause/Resume", command= lambda:self.change_pause_flag())
-            pause_button.pack()
+            pause_button = tk.Button(root, text="Pause/Resume", command= lambda:self.flip_pause_flag())
+            pause_button.place(x=225, y=35)
 
         def Go_back_ten_sentences():
             go_back_button = tk.Button(root, text = "Go Back 10 Sentences.", command= lambda: self.go_back_x_sentences(10))
-            go_back_button.pack()
+            go_back_button.place(x=450, y=5)
 
         def Increment_num_words_output():
             increment_num_words = tk.Button(root, text = "Add 1 Word per Output", command= lambda: self.adjust_num_words(1))
-            increment_num_words.pack()
+            increment_num_words.place(x=450, y=35)
 
         def Decrement_num_words_output():
             decrement_num_words = tk.Button(root, text = "Remove 1 Word per Output", command= lambda: self.adjust_num_words(-1))
-            decrement_num_words.pack()
+            decrement_num_words.place(x=450, y=65)
 
         def Draw():
             global text
@@ -156,11 +154,10 @@ class Speed_reader:
                 
             text.configure(text=self.content, foreground="red") if self.end_of_sentence_flag  else text.configure(text=self.content, foreground = "black")
             
-            root.after(self.text_speed, Refresher)
-
+            root.after(self.refresh_rate, Refresher)
 
         root = tk.Tk()
-        root.geometry("500x500")
+        root.geometry("700x500")
         Draw()
         Refresher()
         root.mainloop()
